59daa60490479eb0606ba6fbca1ba91094427b4a
William Rust <willr@netflix.com>
2017-03-19 19:56:08 -0700

    Updates MovieArrayAdapter to follow the holder pattern.

--- Flicks/app/src/main/java/com/netflix/prototypes/willr/flicks/adapters/MovieArrayAdapter.java
+++ Flicks/app/src/main/java/com/netflix/prototypes/willr/flicks/adapters/MovieArrayAdapter.java
@@ -17,6 +17,9 @@ import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
+import static com.netflix.prototypes.willr.flicks.R.id.tvOverview;
+import static com.netflix.prototypes.willr.flicks.R.id.tvTitle;
+
 /**
  * Created by willr on 3/17/17.
  */
@@ -31,37 +34,52 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Movie movie = getItem(position);
+        ViewHolder viewHolder;
 
         // Check for a cached view
         if (convertView == null) {
+
             // Inflate a new view
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            // Init ViewHolder, find and retain view instances within.
+            viewHolder = new ViewHolder();
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(tvOverview);
+
+            // Cache the viewHolder object inside the view
+            convertView.setTag(viewHolder);
+        } else {
+            // View is being recycled, retrieve the viewHolder object from tag
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        // Find the image view
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
-
-        // Clear it out
-        ivImage.setImageResource(0);
-
-        // Find the text views
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+        // Remove any existing image from the image view
+        viewHolder.ivImage.setImageResource(0);
 
         // Populate the text views
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
 
         // Load the appropriate image for the current orientation
+        // Swapping out the image seemed to provide a better transition than using two layouts.
         int orientation = getContext().getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+            Picasso.with(getContext()).load(movie.getPosterPath()).into(viewHolder.ivImage);
         } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Picasso.with(getContext()).load(movie.getBackdropPath()).into(ivImage);
+            Picasso.with(getContext()).load(movie.getBackdropPath()).into(viewHolder.ivImage);
         }
 
         // Return the view
         return convertView;
+    }
+
+    // View lookup cache
+    private static class ViewHolder {
+        ImageView ivImage;
+        TextView tvTitle;
+        TextView tvOverview;
     }
 }
